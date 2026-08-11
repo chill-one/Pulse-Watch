b7330e867cc110794bb71cb17304de495826e2c8
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-10 21:42:56 -0400

    Implemented: The two minimal Spring Boot entry point for Backend and Worker to initate the MVN Build

---
+++ backend/src/main/java/com/pulsewatch/backend/PulseWatchApplication.java
@@ -0,0 +1,12 @@
+package com.pulsewatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseWatchApplication.class, args);
+    }
+}
