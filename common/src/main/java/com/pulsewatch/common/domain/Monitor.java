bb1b66c03e002b10efff637f1a4f360d08e2cb2f
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-13 23:53:09 -0400

    Renamed: The file Mointor.java to Mointor.java

--- common/src/main/java/com/pulsewatch/common/domain/Monitor.java
+++ common/src/main/java/com/pulsewatch/common/domain/Monitor.java
@@ -1,11 +1,16 @@
 package com.pulsewatch.common.domain;
 
+import java.time.Instant;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 
 
 
@@ -15,7 +20,36 @@ public class Monitor {
 
     //@ID -> This field is the primary identifier for each Monitor
     //@GeneratedValue(strategy = GenerationType.UUID) -> JPA should generate the UUID for us when new Monitor is persisited.
+    //@Column(nullable = False) -> the row of this column cannot be null
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private int checkIntervalSeconds;
+
+    @Column(nullable = false)
+    private int timeoutSeconds;
+
+    @Column(nullable = false)
+    private Instant nextCheckAt;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MonitorStatus status;
+
+    
+
+
 }
