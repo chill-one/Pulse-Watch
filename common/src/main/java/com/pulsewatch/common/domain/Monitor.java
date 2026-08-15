f14dc2c955720bfd077c42eebcbb3cea479aa598
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-14 20:42:00 -0400

    Updated(Monitor): Added the neccesary code for JPA to create a Mointor obj

--- common/src/main/java/com/pulsewatch/common/domain/Monitor.java
+++ common/src/main/java/com/pulsewatch/common/domain/Monitor.java
@@ -17,10 +17,10 @@ import jakarta.persistence.Id;
 //@Entity -> This java class represents presistent data that should be mapped to database table
 @Entity
 public class Monitor {
-
     //@ID -> This field is the primary identifier for each Monitor
     //@GeneratedValue(strategy = GenerationType.UUID) -> JPA should generate the UUID for us when new Monitor is persisited.
     //@Column(nullable = False) -> the row of this column cannot be null
+    //@Enumerated(EnumType.STRING) -> Tells JPA to store the enum using its name 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -49,7 +49,65 @@ public class Monitor {
     @Column(nullable = false)
     private MonitorStatus status;
 
-    
+    @Column(nullable = false)
+    private int consecutiveFailureCount;
 
+    protected Monitor(){}
 
+    public Monitor(
+        String name,
+        String url,
+        int checkIntervalSeconds,
+        int timeoutSeconds,
+        Instant nextCheckAt,
+        MonitorStatus status,
+        int consecutiveFailureCount,
+        Instant createdAt) {
+
+    this.name = name;
+    this.url = url;
+    this.checkIntervalSeconds = checkIntervalSeconds;
+    this.timeoutSeconds = timeoutSeconds;
+    this.nextCheckAt = nextCheckAt;
+    this.status = status;
+    this.consecutiveFailureCount = consecutiveFailureCount;
+    this.createdAt = createdAt;
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getCheckIntervalSeconds() {
+        return checkIntervalSeconds;
+    }
+
+    public int getTimeoutSeconds() {
+        return timeoutSeconds;
+    }
+
+    public Instant getNextCheckAt() {
+        return nextCheckAt;
+    }
+
+    public MonitorStatus getStatus() {
+        return status;
+    }
+
+    public int getConsecutiveFailureCount() {
+        return consecutiveFailureCount;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
 }
