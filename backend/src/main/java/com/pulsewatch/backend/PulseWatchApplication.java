7ff1e76885dea6dc187182fa9ae33bc7e3b71bc3
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-14 22:07:22 -0400

    Updated: explicitly tell Spring where the Enitity lives and the repository scanning package

--- backend/src/main/java/com/pulsewatch/backend/PulseWatchApplication.java
+++ backend/src/main/java/com/pulsewatch/backend/PulseWatchApplication.java
@@ -2,8 +2,16 @@ package com.pulsewatch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 
 @SpringBootApplication
+//Tells Spring that my @Entity classes are over here.
+@EntityScan(basePackages = "com.pulsewatch.common.domain")
+//Tells Spring Data repository interface are ove here.
+@EnableJpaRepositories(basePackages = "com.pulsewatch.persistence.repository")
 public class PulseWatchApplication {
 
     public static void main(String[] args) {
--- worker/src/main/java/com/pulsewatch/worker/PulseWatchWorkerApplication.java
+++ worker/src/main/java/com/pulsewatch/worker/PulseWatchWorkerApplication.java
@@ -2,8 +2,12 @@ package com.pulsewatch.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.pulsewatch.common.domain")
+@EnableJpaRepositories(basePackages = "com.pulsewatch.persistence.repository")
 public class PulseWatchWorkerApplication {
 
     public static void main(String[] args) {
