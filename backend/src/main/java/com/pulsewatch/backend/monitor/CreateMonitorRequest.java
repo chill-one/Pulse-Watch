d2b8826294b896a9346ad64b4b5b53bb079d14e6
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-17 22:25:22 -0400

    Documentation(CreateMointorRequest): Added the neccesary info about the annotation used

--- backend/src/main/java/com/pulsewatch/backend/monitor/CreateMonitorRequest.java
+++ backend/src/main/java/com/pulsewatch/backend/monitor/CreateMonitorRequest.java
@@ -5,12 +5,15 @@ import jakarta.validation.constraints.Positive;
 
 public record CreateMonitorRequest(
 
+        //@NotBlank -> for String Values and Rejects null, "", and whitespace-only string
+
         @NotBlank(message = "Name is requried")
         String name,
 
         @NotBlank(message = "Url is required")
         String url,
 
+        //@Postive -> numeric value must be > 0
         @Positive(message = "CheckIntervalSeconds must be greater than 0")
         int checkIntervalSeconds,
 
