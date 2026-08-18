fc48d3e8081b18f22d7adfd16d0b316434e1ca19
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-18 00:11:51 -0400

    Updated(MointroRepository): Contains a method which spring data can derive the queries from repository method names

--- persistence/src/main/java/com/pulsewatch/persistence/repository/MonitorRepository.java
+++ persistence/src/main/java/com/pulsewatch/persistence/repository/MonitorRepository.java
@@ -1,5 +1,7 @@
 package com.pulsewatch.persistence.repository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +13,21 @@ import com.pulsewatch.common.domain.Monitor;
 //Monitor -> entity this repository manages
 //UUID -> type of Mointor's primary key
 //provides us with save(), findByID(), findAll(), delete(), existsById()
-public interface MonitorRepository
-        extends JpaRepository<Monitor, UUID> {
+public interface MonitorRepository extends JpaRepository<Monitor, UUID> {
+
+
+    /**
+     * Top 200 -> don't grab every due mointor at once
+     * ByNextCheckAtLessThanEqual -> nextCheckAt <= now
+     * OrderByNextCheckAtAsc -> oldest overdue mointors first
+     * @param now Current time
+     * @return All the mointor that are due
+     */
+    List<Monitor>
+        findTop200ByNextCheckAtLessThanEqualOrderByNextCheckAtAsc(
+            Instant now
+        );
+
+
+        
 }
