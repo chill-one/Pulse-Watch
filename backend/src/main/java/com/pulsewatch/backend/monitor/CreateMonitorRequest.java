a6b71132c34f4dbd7d61b680cd76ac882df267d8
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-17 22:21:31 -0400

    Updated(CreateMointorRequest): Added constraints validation for the fields

--- backend/src/main/java/com/pulsewatch/backend/monitor/CreateMonitorRequest.java
+++ backend/src/main/java/com/pulsewatch/backend/monitor/CreateMonitorRequest.java
@@ -1,9 +1,20 @@
 package com.pulsewatch.backend.monitor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public record CreateMonitorRequest(
+
+        @NotBlank(message = "Name is requried")
         String name,
+
+        @NotBlank(message = "Url is required")
         String url,
+
+        @Positive(message = "CheckIntervalSeconds must be greater than 0")
         int checkIntervalSeconds,
+
+        @Positive(message = "TimeoutSeconds must be greater than 0")
         int timeoutSeconds
 ) {
 }
