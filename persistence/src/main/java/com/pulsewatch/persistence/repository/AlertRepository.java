ffc17da8e16cb74f3620e333428c1991963fb174
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-19 20:23:12 -0400

    Updated(AlertRepository): Added a function which returns the top 50 oldest orderd sorted by the given delivery status.

--- persistence/src/main/java/com/pulsewatch/persistence/repository/AlertRepository.java
+++ persistence/src/main/java/com/pulsewatch/persistence/repository/AlertRepository.java
@@ -1,11 +1,24 @@
 package com.pulsewatch.persistence.repository;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pulsewatch.common.domain.Alert;
+import com.pulsewatch.common.domain.DeliveryStatus;
 
 public interface AlertRepository
         extends JpaRepository<Alert, UUID> {
+        
+
+        /**
+         * find 
+         * top 50 -> process at most 50 at a time
+         * ByDeliveryStatus -> Where delivery_status = ?
+         * OrderdByCreatedAtAsc -> oldest alerts first
+         */
+        List<Alert> findTop50ByDeliveryStatusOrderByCreatedAtAsc(
+                DeliveryStatus deliveryStatus
+        );
 }
