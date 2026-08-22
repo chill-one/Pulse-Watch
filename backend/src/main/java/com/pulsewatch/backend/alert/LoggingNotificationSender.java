e35e4bc944f3b8cdc7d896816f6fa680620a314b
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-22 15:15:02 -0400

    Refactor LoggingNotificationSender to use NotificationMessage instead of Alert

--- backend/src/main/java/com/pulsewatch/backend/alert/LoggingNotificationSender.java
+++ backend/src/main/java/com/pulsewatch/backend/alert/LoggingNotificationSender.java
@@ -3,20 +3,18 @@ package com.pulsewatch.backend.alert;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import com.pulsewatch.common.domain.Alert;
-
 @Component
 @Profile("!ses-email")
 public class LoggingNotificationSender
         implements NotificationSender {
 
     @Override
-    public void send(Alert alert) {
+    public void send(NotificationMessage message) {
 
         System.out.println(
                 "Sending notification:"
-                + " alertId=" + alert.getId()
-                + " type=" + alert.getType()
+                + " alertId=" + message.alertId()
+                + " subject=" + message.subject()
         );
     }
 }
