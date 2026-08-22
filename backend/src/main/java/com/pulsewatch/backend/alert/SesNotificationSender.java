c13b7d080a86d2082be484d32e201cfa8296d3ad
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-22 14:22:11 -0400

    Refactor SES email sending logic to use NotificationMessage and simplify code structure

--- backend/src/main/java/com/pulsewatch/backend/alert/SesNotificationSender.java
+++ backend/src/main/java/com/pulsewatch/backend/alert/SesNotificationSender.java
@@ -4,11 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import com.pulsewatch.common.domain.Alert;
-import com.pulsewatch.common.domain.AlertType;
-import com.pulsewatch.common.domain.Incident;
-import com.pulsewatch.common.domain.Monitor;
-
 import software.amazon.awssdk.services.sesv2.SesV2Client;
 import software.amazon.awssdk.services.sesv2.model.Body;
 import software.amazon.awssdk.services.sesv2.model.Content;
@@ -39,108 +34,51 @@ public class SesNotificationSender
 
 
         @Override
-        public void send(Alert alert) {
+        public void send(NotificationMessage message) {
 
-                Incident incident = alert.getIncident();
-                Monitor monitor = incident.getMonitor();
+        Destination destination =
+                Destination.builder()
+                        .toAddresses(toAddress)
+                        .build();
 
-                String subject = buildSubject(alert, monitor);
-                String body = buildBody(alert, monitor, incident);
+        Content subjectContent =
+                Content.builder()
+                        .data(message.subject())
+                        .build();
 
-                Destination destination =
-                        Destination.builder()
-                                .toAddresses(toAddress)
-                                .build();
+        Content bodyContent =
+                Content.builder()
+                        .data(message.body())
+                        .build();
 
-                Content subjectContent =
-                        Content.builder()
-                                .data(subject)
-                                .build();
+        Message emailMessage =
+                Message.builder()
+                        .subject(subjectContent)
+                        .body(
+                                Body.builder()
+                                        .text(bodyContent)
+                                        .build()
+                        )
+                        .build();
 
-                Content bodyContent =
-                        Content.builder()
-                                .data(body)
-                                .build();
+        SendEmailRequest request =
+                SendEmailRequest.builder()
+                        .fromEmailAddress(fromAddress)
+                        .destination(destination)
+                        .content(
+                                EmailContent.builder()
+                                        .simple(emailMessage)
+                                        .build()
+                        )
+                        .build();
 
-                Body emailBody =
-                        Body.builder()
-                                .text(bodyContent)
-                                .build();
+        SendEmailResponse response =
+                sesClient.sendEmail(request);
 
-                Message message =
-                        Message.builder()
-                                .subject(subjectContent)
-                                .body(emailBody)
-                                .build();
-
-                EmailContent emailContent =
-                        EmailContent.builder()
-                                .simple(message)
-                                .build();
-
-                SendEmailRequest request =
-                        SendEmailRequest.builder()
-                                .fromEmailAddress(fromAddress)
-                                .destination(destination)
-                                .content(emailContent)
-                                .build();
-
-                SendEmailResponse response =
-                        sesClient.sendEmail(request);
-
-                System.out.println(
-                        "SES email sent:"
-                        + " alertId=" + alert.getId()
-                        + " messageId=" + response.messageId()
-                );
+        System.out.println(
+                "SES email sent:"
+                + " alertId=" + message.alertId()
+                + " messageId=" + response.messageId()
+        );
         }
-
-
-    private String buildSubject(
-            Alert alert,
-            Monitor monitor) {
-
-        if (alert.getType() == AlertType.OUTAGE) {
-            return "[PulseWatch] DOWN: " + monitor.getName();
-        }
-
-        return "[PulseWatch] RECOVERED: " + monitor.getName();
-    }
-
-    private String buildBody(
-        Alert alert,
-        Monitor monitor,
-        Incident incident) {
-
-    if (alert.getType() == AlertType.OUTAGE) {
-
-        return """
-                PulseWatch detected an outage.
-
-                Monitor: %s
-                URL: %s
-                Status: DOWN
-                Outage started: %s
-                """.formatted(
-                        monitor.getName(),
-                        monitor.getUrl(),
-                        incident.getStartedAt()
-                );
-    }
-
-    return """
-            PulseWatch detected a recovery.
-
-            Monitor: %s
-            URL: %s
-            Status: UP
-            Outage started: %s
-            Recovered at: %s
-            """.formatted(
-                    monitor.getName(),
-                    monitor.getUrl(),
-                    incident.getStartedAt(),
-                    incident.getEndedAt()
-            );
-    }
 }
