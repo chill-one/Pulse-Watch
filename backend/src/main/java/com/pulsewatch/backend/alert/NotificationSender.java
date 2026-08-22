692264028cb569a147d8ce56602d17a8d7e2acc1
Suprim Bhandari <suprim.b@yahoo.com>
2026-08-22 14:22:25 -0400

    Update NotificationSender interface to use NotificationMessage instead of Alert

--- backend/src/main/java/com/pulsewatch/backend/alert/NotificationSender.java
+++ backend/src/main/java/com/pulsewatch/backend/alert/NotificationSender.java
@@ -1,8 +1,6 @@
 package com.pulsewatch.backend.alert;
 
-import com.pulsewatch.common.domain.Alert;
-
 public interface NotificationSender {
 
-    void send(Alert alert);
+    void send(NotificationMessage message);
 }
